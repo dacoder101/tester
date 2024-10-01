de492ca537c3371171dffb8b6fe09057560ef6ae
dacoder <bobbye23899@gmail.com>
2024-09-30 21:37:41 -0700

    Random staggered commits

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,8 @@
-
+function displayDiv(divId) {
+    let div = document.getElementById(divId);
+    if (div.style.display === "none") {
+        div.style.display = "block";
+    } else {
+        div.style.display = "none";
+    }
+}
