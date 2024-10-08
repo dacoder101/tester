1c4e0e245c0de46ceaa86707c2390d4bc06bf73a
dacoder <bobbye23899@gmail.com>
2024-10-08 03:18:57 +0000

    Reload fix

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,9 @@
 const validator = new JSONValidator("json-input");
 
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("json-input").value = ""; // Solves a weird issue upon reloading the page
+});
+
 function initiateTest() {
     if (validator.testReady) {
         console.log("Test is ready to start.");
